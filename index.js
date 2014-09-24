6fb922bfe3070f3d49bca6819bfaf72fb76ac78c
psirenny <djtorres0@gmail.com>
2014-09-24 17:56:28 -0400

    remove view property

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function Component () {}
 
-Component.prototype.view = __dirname;
-
 Component.prototype.create = function () {
   var period = this.model.get('period') || 1000;
   this.timer = setInterval(this.tick.bind(this), period);
